4628f086ac0f401c958b85f9679f8daee692aa08
gouthampai05 <gouthampai05@gmail.com>
2024-06-23 00:44:54 +0530

    Fixed route again

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ var listener = app.listen(process.env.PORT || 3000, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
 
-app.get("/whoami", (req, res) => {
+app.get("/api/whoami", (req, res) => {
   const ipaddress = req.ip.split(":")[3];
   const language = req.get("accept-language");
   const software = req.get("user-agent");
